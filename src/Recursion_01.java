a4c0c7a485c8da822071dbb2f35f8519186b7d60
Sandip <hari.sandip123@gmail.com>
2023-01-25 14:16:40 +0530

    my first commit

---
+++ src/Recursion_01.java
@@ -0,0 +1,27 @@
+
+     import java.util.*;
+    public class Recursion_01 {
+
+
+        static int count = 0;
+
+        public static int countDigits(int num) {
+            if (num > 0) {
+                count++;
+                countDigits(num / 10);
+            }
+
+            return count;
+        }
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int num = 10000;
+            int res = 0;
+
+            res = countDigits(num);
+            System.out.printf("Total digits are: " + res);
+        }
+    }
+
